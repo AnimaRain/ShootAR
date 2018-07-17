4b7a25a82eed5da803eadf22335cdc87079d146b
AnimaRain <ioannis_spyr@yahoo.gr>
2018-07-17 20:29:56 +0300

    Create a player unit tests blueprint

---
+++ Assets/Tests/PlayerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using ShootAR;
+
+class PlayerTest
+{
+	/*[UnityTest]
+	public IEnumerator PlayerShoots()
+	{
+
+	}
+
+	[UnityTest]
+	public IEnumerator PlayerDiesWhenHealthDepleted()
+	{
+
+	}
+
+	[UnityTest]
+	public IEnumerator ArmorProtects()
+	{
+
+	}*/
+}
