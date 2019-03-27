235cbc4679faef7199a41563b09274f036a2f9bd
AnimaRain <ioannis_spyr@yahoo.gr>
2019-03-27 23:43:19 +0200

    Create scene for testing persistent camera
    For the new menu we need to make the feedback of the device's camera
    to persist through the scene changing and not be destroyed. This commit
    does not need to be merged into master and is intended for testing
    purposes only.
    
    The first scene contains the background image and the controller.
    The second scene contains only a message that when seen, it means that
    the test was successful.
    
    Signed-off-by: AnimaRain <ioannis_spyr@yahoo.gr>

---
+++ Assets/Scripts/PersistentCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersistentCamera : MonoBehaviour
+{
+	/* The canvas containing the raw image; it must be assigned through
+	 * the inspector. We only need the image but the image is required to be
+	 * child of the canvas and only the root object can be instructed to not
+	 * get destroyed when the scene changes. That's why we get the canvas
+	 * instead of the image. */
+	[SerializeField] private Canvas backgroundCanvas;
+	private WebCamTexture cameraCapture;
+
+	private void Awake() {
+		cameraCapture = new WebCamTexture();
+
+		/* The object with this script and the canvas must be protected
+		 * to survive the new scene being loaded. */
+		DontDestroyOnLoad(backgroundCanvas.gameObject);
+		DontDestroyOnLoad(gameObject);
+
+		/* The image required is extracted from the canvas container. */
+		UnityEngine.UI.RawImage background =
+			backgroundCanvas.GetComponentInChildren<UnityEngine.UI.RawImage>();
+
+		/* The camera capture freezes when a new scene is loaded.
+		 * Calling Play after the scene is loaded seems to fix that.
+		 * sceneLoaded is called before Start. */
+		SceneManager.sceneLoaded += (Scene scene, LoadSceneMode mode) => {
+			cameraCapture.Play();
+		};
+		background.texture = cameraCapture;
+	}
+
+	/* Create a button that loads the new scene when pressed. */
+	private void OnGUI() {
+		if (GUI.Button(new Rect(10, 10, 20, 20), "O"))
+			SceneManager.LoadScene(1);
+	}
+}
