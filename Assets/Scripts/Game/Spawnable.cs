44a67dd5ce92b51feacc4a63253930f388c1b483
AnimaRain <ioannis_spyr@yahoo.gr>
2019-08-05 00:41:00 +0300

    Create some basic code for object pools

--- Assets/Scripts/Game/Spawnable.cs
+++ Assets/Scripts/Game/Spawnable.cs
@@ -1,8 +1,4 @@
-﻿/* The reason why this class exists is because Spawner.objectToSpawn can not
- * be assigned an interface through Unity's Inspector. Best solution was to
- * use a class that is parent of every object that can be spawned from a
- * spawner. */
-
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace ShootAR
@@ -30,5 +26,51 @@ namespace ShootAR
 		public GameState GameState {
 			set { gameState = value; }
 		}
+
+		/// <summary>
+		/// A container of object pools.
+		/// </summary>
+		private static Dictionary<System.Type, Stack<Spawnable>> pools;
+
+		/// <summary>
+		/// Get the types of available object pools.
+		/// </summary>
+		/// <returns>an array containing the types</returns>
+		public static System.Type[] GetPools() {
+			System.Type[] types = new System.Type[pools.Count];
+			pools.Keys.CopyTo(types, 0);
+			return types;
+		}
+
+		/// <summary>
+		/// Request an object of type <typeparamref name="T"/> from the
+		/// appropriate pool.
+		/// </summary>
+		/// <typeparam name="T">The type of object to match to a pool</typeparam>
+		/// <returns>
+		/// reference to an available object of type <typeparamref name="T"/>
+		/// </returns>
+		public static T RequestObject<T>() where T : Spawnable {
+			//TODO: Check if there are available objects.
+			//TODO: Expand and repopulate pool if necessary.
+			T availableObject = (T)pools[typeof(T)].Pop();
+			//TODO: Create Initialize method
+			availableObject.ResetState();
+			return availableObject;
+		}
+
+		/// <summary>
+		/// Instead of destroying the object, deactivates and returns it to the pool
+		/// to be used again when needed.
+		/// </summary>
+		public void ReturnToPool() {
+			gameObject.SetActive(false);
+			pools[GetType()].Push(this);
+		}
+
+		/// <summary>
+		/// Return the object to an "as new" state so it can be used again.
+		/// </summary>
+		protected virtual void ResetState() {}
 	}
 }
