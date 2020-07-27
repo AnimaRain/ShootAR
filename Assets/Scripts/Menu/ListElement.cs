1d4ffdb1136dd64abccd6da7f1a93bc9e4741cb8
AnimaRain <ioannis_spyr@yahoo.gr>
2020-07-27 20:11:25 +0300

    Create WaveEditor

---
+++ Assets/Scripts/Menu/ListElement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace ShootAR.Menu {
+	[RequireComponent(typeof(Text))]
+	public class ListElement : MonoBehaviour {
+		private static Color unselectedBackground = new Color(0.32f, 0.35f, 0.42f, 0.25f);
+		private static Color selectedBackground = new Color(0.54f, 0.44f, 0.32f, 0.25f);
+
+		private static int elementsCount = 0;
+
+		public int ID { get; set; }
+
+		private Text uiText;
+
+		private bool selected;
+
+		public bool Selected {
+			get => selected;
+			set {
+				selected = value;
+
+				uiText.GetComponentInChildren<Image>().color =
+					value ? selectedBackground : unselectedBackground;
+
+				if (value) {
+					GetComponentInParent<ListSelectionController>()
+						.ChangeSelection(ID);
+				}
+			}
+		}
+
+		public void SetText(string value) => uiText.text = value;
+
+		public void Start() {
+			if (uiText == null) uiText = GetComponent<Text>();
+
+			ID = elementsCount++;
+
+			SetText("Dragon");
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/ListSelectionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+namespace ShootAR.Menu {
+	public class ListSelectionController : MonoBehaviour
+	{
+		private ListElement selected;
+
+		public void Start() {
+			//TODO: Fetch from configurations' file which pattern is selected.
+		}
+
+		public void ChangeSelection(int id) {
+			if (selected?.ID == id) return;
+
+			if (selected != null)
+				selected.Selected = false;
+
+			selected = Array.Find(
+				GetComponentsInChildren<ListElement>(),
+				element => element.ID == id
+			);
+		}
+	}
+}
